4d8519ef6e80c3905aa0e565532017fd25ab65a9
Thierry Chau <thierry.chau@gmail.com>
2023-09-26 15:07:19 +0000

    created payment route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,4 +19,27 @@ class PagesController < ApplicationController
     render({ :template => "pages/square_root_result"})
   end
 
+  def payment
+    render({ :template => "pages/payment" })
+  end
+
+  def payment_result
+    @apr = params.fetch("user_apr").to_f / 100
+    @years = params.fetch("user_years").to_i
+    @principal = params.fetch("user_pv").to_i
+  
+    @number_periods = 12*@years
+    @rate_per_period = @apr/12
+  
+    @payment = (@rate_per_period * @principal) / (1 - (1 + @rate_per_period)**(-@number_periods))
+    render({ :template => "pages/payment_result"})
+  end
+
+  def random
+    render({ :template => "pages/random" })
+  end
+
+  def random_resulst
+    render({ :template => "pages/random_resulst"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
 
   get("/square_root/new", { :controller => "pages", :action => "square_root" })
   get("/square_root/results", { :controller => "pages", :action => "square_root_result" })
+
+  get("/payment/new", { :controller => "pages", :action => "payment" })
+  get("/payment/results", { :controller => "pages", :action => "payment_result" })
+
+  get("/random/new", { :controller => "pages", :action => "random" })
+  get("/random/results", { :controller => "pages", :action => "random_result" })
 end
